bb796ad9102e90c9d601f82446789f207a8be394
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-04-12 23:01:03 -0400

    Initialize project.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Todo_App_WEB_1001.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
